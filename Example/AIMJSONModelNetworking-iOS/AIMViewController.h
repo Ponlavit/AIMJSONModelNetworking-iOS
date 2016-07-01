6f3cf92c810fc722b92303d3f86459321fb238f0
Tzoani <darksky_ddd@hotmail.com>
2016-07-01 08:59:38 +0700

    Initial Cocoapods

---
+++ Example/AIMJSONModelNetworking-iOS/AIMViewController.h
@@ -0,0 +1,13 @@
+//
+//  AIMViewController.h
+//  AIMJSONModelNetworking-iOS
+//
+//  Created by Tzoani on 06/30/2016.
+//  Copyright (c) 2016 Tzoani. All rights reserved.
+//
+
+@import UIKit;
+
+@interface AIMViewController : UIViewController
+
+@end
---
+++ Example/Pods/Target Support Files/Pods-AIMJSONModelNetworking-iOS_Tests/Pods-AIMJSONModelNetworking-iOS_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_AIMJSONModelNetworking_iOS_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AIMJSONModelNetworking_iOS_TestsVersionString[];
+

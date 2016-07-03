81d47675abb6703c12297da466132dafdefc2f2f
Tzoani <darksky_ddd@hotmail.com>
2016-07-03 11:13:29 +0700

    Clear all file

--- Example/Pods/Target Support Files/AIMJSONModelNetworking-iOS/AIMJSONModelNetworking-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AIMJSONModelNetworking_iOS : NSObject
-@end
-@implementation PodsDummy_AIMJSONModelNetworking_iOS
-@end
--- Example/Pods/Target Support Files/AIMJSONModelNetworking-iOS/AIMJSONModelNetworking-iOS-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double AIMJSONModelNetworking_iOSVersionNumber;
-FOUNDATION_EXPORT const unsigned char AIMJSONModelNetworking_iOSVersionString[];
-
